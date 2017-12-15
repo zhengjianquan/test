50c4873d5303ab63d67ef02bc47a9514345e53e9
mc2 <y464356928@qq.com>
2017-12-16 01:06:51 +0800

    test

---
+++ main.js
@@ -0,0 +1,9 @@
+// import './css/style.css';
+// import './css/style1.less';
+//import '../js/template.js';
+//import qqlogo from  '../asset/js';
+//console.log(qqlogo);
+//$('body').html('<img src="'+qqlogo+'">');
+
+// import './detail.js';
+// import 'vue';
